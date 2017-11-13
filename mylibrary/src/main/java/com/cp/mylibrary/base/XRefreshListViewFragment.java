9c59f2492ea060765ddb7138d006ca6a2936876f
467471641@qq.com <467471641@qq.com>
2017-11-13 16:35:27 +0800

    fragment 加上头部

--- mylibrary/src/main/java/com/cp/mylibrary/base/XRefreshListViewFragment.java
+++ mylibrary/src/main/java/com/cp/mylibrary/base/XRefreshListViewFragment.java
@@ -176,6 +176,7 @@ public class XRefreshListViewFragment<T extends MyEntity> extends MyBaseFragment
 
         if (refresh) {
             if (NetWorkUtil.hasInternetConnected(getActivity())) {
+                LogCp.i(LogCp.CP, XRefreshListViewFragment.class + "  来刷新了 " + cacheStr);
 
                 requestData();
 
