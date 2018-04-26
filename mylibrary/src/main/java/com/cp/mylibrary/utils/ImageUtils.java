4643347b81a21d6d861a5ef129708785318626e8
467471641@qq.com <467471641@qq.com>
2018-04-26 15:40:53 +0800

    fragment 加上头部

--- mylibrary/src/main/java/com/cp/mylibrary/utils/ImageUtils.java
+++ mylibrary/src/main/java/com/cp/mylibrary/utils/ImageUtils.java
@@ -24,6 +24,8 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -303,6 +305,69 @@ public class ImageUtils {
         return height;
     }
 
+
+
+    /**
+     * 保存bitmap到SD卡
+     *
+     * @param bitName 保存的名字
+     * @param mBitmap 图片对像
+     *                return 生成压缩图片后的图片路径
+     */
+    public static String saveMyBitmap(String bitName, Bitmap mBitmap) {
+        File f = new File(SDCardUtils.getSDCardPath() + bitName + ".png");
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            System.out.println("在保存图片时出错：" + e.toString());
+        }
+        FileOutputStream fOut = null;
+        try {
+            fOut = new FileOutputStream(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            mBitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+        } catch (Exception e) {
+            return "create_bitmap_error";
+        }
+        try {
+            fOut.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "/sdcard/" + bitName + ".png";
+    }
+
+    /**
+     * 保存bitmap到SD卡
+     *
+     * @param bitmap
+     * @param imagename
+     */
+    public static String saveBitmapToSDCard(Bitmap bitmap, String imagename) {
+        String path = SDCardUtils.getSDCardPath() + "img-" + imagename + ".jpg";
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(path);
+            if (fos != null) {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 90, fos);
+                fos.close();
+            }
+
+            return path;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 
 
